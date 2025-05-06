906666f8a3de4279185144511d90c29f24a9f53e
jayraj1906 <jayrajchaudhary02@gmail.com>
2025-05-06 21:07:32 +0100

    added env

--- src/exception/__init__.py
+++ src/exception/__init__.py
@@ -1,48 +1,39 @@
 import sys
 import logging
 
-def error_message_detail(error: Exception, error_detail: sys) -> str:
-    """
-    Extracts detailed error information including file name, line number, and the error message.
-
-    :param error: The exception that occurred.
-    :param error_detail: The sys module to access traceback details.
-    :return: A formatted error message string.
-    """
-    # Extract traceback details (exception information)
-    _, _, exc_tb = error_detail.exc_info()
-
-    # Get the file name where the exception occurred
-    file_name = exc_tb.tb_frame.f_code.co_filename
-
-    # Create a formatted error message string with file name, line number, and the actual error
-    line_number = exc_tb.tb_lineno
-    error_message = f"Error occurred in python script: [{file_name}] at line number [{line_number}]: {str(error)}"
-    
-    # Log the error for better tracking
-    logging.error(error_message)
-    
-    return error_message
+def error_message_detail(error:Exception,error_detail:sys) ->str:
+	"""
+	Extracts detailed error information including file name, line number and the error message
+	Params:
+		error_details: The sys module to access traceback details
+		error: The exception that occured
+	Return:
+		A formatted error message string
+	"""
+	_,_,exc_tb=error_detail.exc_info()
+	file_name=exc_tb.tb_frame.f_code.co_filename
+	line_number=exc_tb.tb_lineno
+	error_message=f"Error occured in python script: [{file_name}] at line number [{line_number}]: {str(error)}"
+	logging.error(error_message)
+	return error_message
 
 class MyException(Exception):
-    """
-    Custom exception class for handling errors in the US visa application.
-    """
-    def __init__(self, error_message: str, error_detail: sys):
-        """
-        Initializes the USvisaException with a detailed error message.
+	"""
+	Custom exception class for handling errors in the Vehicle insurance prediction
+	"""
+	def __init__(self,error_message:str,error_detail:sys):
+		"""
+		Initializes the Vehicle insurance prediction exception with a detailed error
+		Params:
+			error_message: A string describing the error.
+			error_details: The sys module to access traceback details
+		"""
 
-        :param error_message: A string describing the error.
-        :param error_detail: The sys module to access traceback details.
-        """
-        # Call the base class constructor with the error message
-        super().__init__(error_message)
+		super().__init__(error_message)
+		self.error_message=error_message_detail(error_message,error_detail)
 
-        # Format the detailed error message using the error_message_detail function
-        self.error_message = error_message_detail(error_message, error_detail)
-
-    def __str__(self) -> str:
-        """
-        Returns the string representation of the error message.
-        """
-        return self.error_message
+	def __str__(self) -> str:
+		"""
+		Returns the string representation of the error message.
+		"""
+		return self.error_message
